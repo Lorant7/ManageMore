35122fcb5e9e4700b57e35cdc5b7bdd93b7003f8
Lorant7 <tomy.lorant@gmail.com>
2023-10-28 00:44:36 -0400

    Updated table creation statemens for the new ER

--- src/SQLscripts/schema.sql
+++ src/SQLscripts/schema.sql
@@ -3,79 +3,74 @@
 -- with JavaScript
 
 -- Figure out what you want for ON UPDATE and ON DELETE
+-- Make different ON UPDATE and ON DELETE actions for
+-- primary and foreign keys
 
 CREATE TABLE Employee(
-    employeeID INT UNIQUE,
-    name VARCHAR(20),
-    job_title VARCHAR(20),
+    employeeID INTEGER UNIQUE,
+    name VARCHAR(50),
+    job_title VARCHAR(50),
     salary DECIMAL(11,2) NOT NULL DEFAULT 0,
-    facilityID INT NOT NULL,
+    facilityID INTEGER NOT NULL,
+    employeeType VARCHAR(50),
     PRIMARY KEY (employeeID),
     FOREIGN KEY (facilityID) REFERENCES Facility(facilityID),
     ON UPDATE CASCADE ON DELETE CASCADE);
 
 CREATE TABLE Facility(
-    facilityID INT UNIQUE,
-    name VARCHAR(20),
+    facilityID INTEGER UNIQUE,
+    name VARCHAR(50),
     address VARCHAR(50),
     PRIMARY KEY (facilityID)
     ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE Transfers(
-    start_facilityID INT UNIQUE,
-    end_facilityID INT UNIQUE,
-    vehicleID INT UNIQUE,
-    productID INT,
-    PRIMARY KEY (start_facilityID, end_facilityID, vehicleID)
-    FOREIGN KEY (start_facilityID) REFERENCES Facility(facilityID),
-    FOREIGN KEY (end_facilityID) REFERENCES Facility(facilityID),
-    FOREIGN KEY (vehicleID) REFERENCES Vehicle(vehicleID),
-    ON UPDATE CASCADE ON DELETE CASCADE
-);
-
 CREATE TABLE Stock(
-    productID INT UNIQUE,
-    amount INT DEFAULT 0,
-    desired_amount INT DEFAULT 0,
-    facilityID INT,
-    PRIMARY KEY (productID)
+    productID INTEGER,
+    amount INTEGER DEFAULT 0,
+    desired_amount INTEGER DEFAULT 0,
+    facilityID INTEGER,
+    PRIMARY KEY (productID,facilityID)
     FOREIGN KEY (facilityID) REFERENCES Facility(facilityID),
     ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 -- TODO: makeusre driverID instead of driver
 CREATE TABLE Vehicle(
-    vechicleID INT UNIQUE,
-    type VARCHAR(20),
-    driverID INT UNIQUE,
-    capacity INT DEFAULT 0,
+    vechicleID INTEGER UNIQUE,
+    type VARCHAR(50),
+    driverID INTEGER UNIQUE,
+    capacity INTEGER DEFAULT 0,
     PRIMARY KEY (vehicleID),
+    FOREIGN KEY (driverID) REFERENCES Employee(employeeID),
     ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE Vendor(
-    vendorID INT UNIQUE,
-    name VARCHAR(20) NOT NULL,
+    vendorID INTEGER UNIQUE,
+    name VARCHAR(50) NOT NULL,
     address VARCHAR(50),
     PRIMARY KEY (vendorId),
     ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE Product(
-    productID INT UNIQUE,
-    name VARCHAR(20),
+    productID INTEGER NOT NULL,
+    vendorID INTEGER NOT NULL,
+    name VARCHAR(50),
     description VARCHAR(200),
     image BLOB,
     PRIMARY KEY (productID)
+    FOREIGN KEY (vendorID) REFERENCES Vendor(vendorID),
     ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE Sells(
-    vendorID INT UNIQUE,
-    productID INT UNIQUE,
-    price DECIMAL(7,2)
-    PRIMARY KEY (vendorID, productID)
+    vendorID INTEGER NOT NULL,
+    productID INTEGER NOT NULL,
+    unitPrice DECIMAL(7,2) DEFAULT 0,
+    amount INTEGER DEFAULT 0,
+    PRIMARY KEY (vendorID, productID),
     FOREIGN KEY (vendorID) REFERENCES Vendor(vendorID),
     FOREIGN KEY (productID) REFERENCES Product(productID),
     ON UPDATE CASCADE ON DELETE CASCADE
@@ -83,7 +78,20 @@ CREATE TABLE Sells(
 
 -- Discuss purchases
 CREATE TABLE Purchases(
-    productID INT UNIQUE,
-    amount INT DEFUALT 0,
-    vehicleID INT
+    facilityID INTEGER NOT NULL,
+    productID INTEGER NOT NULL,
+    amount INTEGER DEFUALT 0,
+    PRIMARY KEY(facilityID, porductID),
+    FOREIGN KEY (facilityID) REFERENCES Facility(facilityID),
+    FOREIGN KEY (productID) REFERENCES Product(productID),
+    ON UPDATE CASCADE ON DELETE CASCADE
 );
+
+CREATE TABLE Order(
+    orderID INTEGER UNIQUE,
+    productID INTEGER NOT NULL,
+    amount INTEGER DEFAULT 0,
+    PRIMARY KEY (orderID)
+    FOREIGN KEY (productID) REFERENCES Product(productID),
+    ON UPDATE CASCADE ON DELETE CASCADE
+)
